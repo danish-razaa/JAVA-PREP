5ecd9dc7220bac4cfb7c49c696cc02774e9bc5e1
Danish Raza <iamdanishrazaa@gmail.com>
2026-09-17 02:51:51 +0530

    learning

---
+++ Divide.java
@@ -0,0 +1,8 @@
+public class Divide{
+    public static void main (String [] args){
+        int a = 15;
+        int b = 3;
+        int c =a/b;
+        System.out.println("The division of a and b is :"+c);
+    }
+}
