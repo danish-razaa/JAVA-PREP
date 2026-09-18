ea2ded76d5d37cb283b35b74f302b9c38cf07546
Danish Raza <iamdanishrazaa@gmail.com>
2026-09-19 01:53:27 +0530

    Sequence Char

---
+++ lower.java
@@ -0,0 +1,9 @@
+public class lower{
+    public static void main (String [] args){
+        String str = "Danish Raza is a good boy";
+        System.out.println("\"Danish Raza is a good boy\"");
+        
+
+
+    }
+}
