aecd9e6e6b453c445ebfc332692e953921394889
Danish Raza <iamdanishrazaa@gmail.com>
2026-09-22 23:55:17 +0530

    brea example 2

---
+++ BreakExample2.java
@@ -0,0 +1,13 @@
+public class BreakExample2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i % 3 == 0) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
